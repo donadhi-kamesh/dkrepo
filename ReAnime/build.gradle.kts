b5d0e3e40afafcf1dd0cbde493eef9251a1050d1
donadhi-kamesh <donadhi-kamesh@users.noreply.github.com>
2026-08-05 18:03:37 +0530

    Fix tvTypes property in ReAnime build.gradle.kts

--- ReAnime/build.gradle.kts
+++ ReAnime/build.gradle.kts
@@ -7,11 +7,7 @@ cloudstream {
     language = "en"
     description = "Re:ANIME extension provider for Cloudstream"
     authors = listOf("dkrepo")
-    status = 2 // 1: Down, 2: OK, 3: Slow, 4: Beta
-    types = setOf(
-        com.lagradost.cloudstream3.TvType.Anime,
-        com.lagradost.cloudstream3.TvType.OVA,
-        com.lagradost.cloudstream3.TvType.AnimeMovie
-    )
+    status = 2
+    tvTypes = setOf("Anime", "OVA", "AnimeMovie")
     iconUrl = "https://reanime.to/favicon-32x32.png"
 }
